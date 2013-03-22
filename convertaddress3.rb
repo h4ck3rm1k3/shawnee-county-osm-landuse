4c88dd83b333937304852d789f947b2559c2f4d5
James Michael DuPont <JamesMikeDuPont@gmail.com>
2013-03-22 06:45:01 -0500

    now produce points and ways in separate files

--- convertaddress3.rb
+++
@@ -1,8 +0,0 @@
-#XMin: -10691794.4087113
-#YMin: 4701691.55697822
-#XMax: -10629564.1332868
-#YMax: 4754126.03261384
-#Spatial Reference: 102100 (385
-
-
-#http://gis.snco.us/ArcGIS/rest/services/Basemap_102100/MapServer/export?bbox=-10691794.4087113%2C4701691.55697822%2C-10629564.1332868%2C4754126.03261384&bboxSR=&layers=&layerdefs=&size=&imageSR=&format=png&transparent=false&dpi=&time=&layerTimeOptions=&f=html
