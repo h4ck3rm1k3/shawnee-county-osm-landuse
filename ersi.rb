fa39f00fd23c8b0f6a27acd5aae312610d656716
James Michael DuPont <JamesMikeDuPont@gmail.com>
2014-07-26 17:53:50 -0500

    ersi

---
+++ ersi.rb
@@ -0,0 +1,76 @@
+require 'mechanize'
+require 'json'
+require 'pp'
+require 'filecache'
+
+def layer(agent, cache, url, layer_url)
+  jsonurl = layer_url + '/?f=pjson'
+  old =  cache.get(url)
+  if (old.nil?) 
+    page = agent.get jsonurl
+    result_hash = JSON.parse(page.body)
+    puts "fetched layer",url
+    #pp result_hash
+    cache.set(url, result_hash)
+  else
+    puts "got old",url
+  end  
+end 
+
+def services(agent,cache,  url)
+  jsonurl = url + '/?f=pjson'
+
+  old =  cache.get(url)
+  if (old.nil?) 
+    page = agent.get jsonurl
+    result_hash = JSON.parse(page.body)
+  else
+    puts "got old2",url
+    result_hash = old
+  end  
+
+  puts "process services ", url
+  if result_hash['layers']
+    result_hash['layers'].each { |x| 
+      id = x['id']
+      layer_url = "#{url}/#{id}"
+      puts layer_url
+      layer(agent,cache, url,layer_url)
+      
+    }    
+  end
+end
+
+
+def folders(agent,cache,  base, url)
+  jsonurl = url + '/?f=pjson'
+  old =  cache.get(url)
+  if (old.nil?) 
+    page = agent.get jsonurl
+    result_hash = JSON.parse(page.body)
+  else
+    puts "got old3",url
+    result_hash = old
+  end
+  result_hash['folders'].each { |x| 
+    folder_url = url + '/' + x
+    folders(agent,cache,  url, folder_url)
+  }
+  result_hash['services'].each { |x| 
+    name = x['name']
+    type = x['type']
+    service_url = "#{base}/#{name}/#{type}"
+    puts service_url
+    services(agent, cache, service_url)
+  }
+end
+
+def main()
+  agent = Mechanize.new
+  cache = FileCache.new("ersi",".")
+  baseurl="https://dgco.douglas-county.com/arcgis/rest/services"
+  folders(agent,cache, baseurl, baseurl)
+end
+
+
+main()
